b6b879221fa097ab7864d32ba345c91d7c4486d3
bdoms <ravenize@gmail.com>
2015-05-07 14:24:37 -0500

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+
+def installed():
+    """ detects if the system has git or not """
+    try:
+        subprocess.call(["git", "rev-parse"])
+        return True
+    except OSError:
+        return False
+
+def isRepository():
+    return subprocess.check_output(["git", "rev-parse", "--is-inside-work-tree"]).replace("\n", "") == "true"
+
+def currentBranch():
+    """ gets the name of the current branch """
+    return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).replace("\n", "")
+
+def checkout(branch):
+    """ returns True if successful, False if there was an error """
+    # git is stupid and puts out the success message for the checkout command on stderr instead of stdout
+    process = subprocess.Popen(["git", "checkout", branch], stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    branch_switch = "Switched to branch '%s'" % branch
+    branch_on = "Already on '%s'" % branch
+    if branch_switch not in err and branch_on not in err:
+        raise Exception("Could not switch to branch '%s'. Reason:\n%s" % (branch, err))
+
+def commitDetails(formatting, commit_range):
+    # see http://git-scm.com/book/ch2-3.html for formatting options
+    return subprocess.check_output(['git', 'log', '--pretty=format:"' + formatting + '"', commit_range])
+
+def commitBody(long_sha):
+    body = subprocess.check_output(['git', 'log', '--pretty=format:"%B"', '-n', '1', long_sha])
+    return body[1:-1].strip() # body is surrounded by quotes (e.g. '"commit message"')
+
+def remotesWithCommit(long_sha):
+    return subprocess.check_output(['git', 'branch', '-r', '--contains', long_sha])
+
+def pushForced():
+    pid = os.getppid()
+    push_command = subprocess.check_output(['ps', '-ocommand=', '-p', str(pid)])
+    return ('--force' in push_command or '-f' in push_command)
---
+++ readme.md
@@ -0,0 +1,12 @@
+Copyright &copy; 2015, [Brendan Doms](http://www.bdoms.com/)  
+Licensed under the [MIT license](http://www.opensource.org/licenses/MIT)
+
+# Git
+
+A subprocess based Python interface to git.
+
+## What Is This
+
+Increasingly I find myself scripting interactions with git.
+Although there are many projects to help with this already, none of them exactly fit my needs.
+So I've created this small project and I'll add to it as I need to.
